8aa0fb4603694148e59810e3e6fed4aac81d7a25
LorenR520 <lorenrivas527@gmail.com>
2025-11-23 11:18:28 -0500

    Update MobileSidebar.tsx

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import DocsSidebar from "./DocsSidebar";
+import Link from "next/link";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoClose } from "react-icons/io5";
 
@@ -10,7 +10,7 @@ export default function MobileSidebar() {
 
   return (
     <>
-      {/* ICON BUTTON */}
+      {/* OPEN BUTTON */}
       <button
         onClick={() => setOpen(true)}
         className="md:hidden p-2 text-gray-700 dark:text-gray-300"
@@ -26,20 +26,44 @@ export default function MobileSidebar() {
         />
       )}
 
-      {/* DRAWER */}
+      {/* SLIDE-OUT DRAWER */}
       <div
-        className={`fixed top-0 left-0 h-full z-50 transform transition-transform duration-300 ${
+        className={`fixed top-0 left-0 h-full w-64 bg-white dark:bg-neutral-900 shadow-xl z-50 transform transition-transform duration-300 ${
           open ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        <div className="w-64 h-full bg-white dark:bg-neutral-900 shadow-xl">
-          <div className="flex justify-end p-4">
-            <button onClick={() => setOpen(false)}>
-              <IoClose size={26} className="text-gray-700 dark:text-gray-300" />
-            </button>
-          </div>
-          <DocsSidebar isMobile onClose={() => setOpen(false)} />
+        {/* CLOSE BUTTON */}
+        <div className="flex justify-end p-4">
+          <button onClick={() => setOpen(false)}>
+            <IoClose size={26} className="text-gray-700 dark:text-gray-300" />
+          </button>
         </div>
+
+        {/* MOBILE NAVIGATION LINKS */}
+        <nav className="flex flex-col space-y-4 px-6 text-gray-900 dark:text-gray-200">
+
+          <Link href="/installation" onClick={() => setOpen(false)}>
+            Installation
+          </Link>
+
+          <Link href="/pricing" onClick={() => setOpen(false)}>
+            Pricing
+          </Link>
+
+          <Link href="/providers" onClick={() => setOpen(false)}>
+            Providers
+          </Link>
+
+          {/* AUTH */}
+          <Link href="/auth/login" onClick={() => setOpen(false)}>
+            Login
+          </Link>
+
+          <Link href="/auth/signup" onClick={() => setOpen(false)}>
+            Sign Up
+          </Link>
+
+        </nav>
       </div>
     </>
   );
