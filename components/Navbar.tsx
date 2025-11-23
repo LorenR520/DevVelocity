374865f2b5c66ddf20516937ed678bc8922c56fc
LorenR520 <lorenrivas527@gmail.com>
2025-11-23 10:25:12 -0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,28 @@
 
 import Link from "next/link";
 import MobileSidebar from "./MobileSidebar";
-import { useState, useEffect } from "react";
 import { IoMoon, IoSunny } from "react-icons/io5";
 import SearchBar from "./SearchBar";
+import { supabase } from "../lib/supabase";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+    });
+  }, []);
+
+  async function logout() {
+    await supabase.auth.signOut();
+    window.location.href = "/";
+  }
+
   const [dark, setDark] = useState(false);
 
-  // Load from localStorage on first load
   useEffect(() => {
     const stored = localStorage.getItem("theme");
     if (stored === "dark") {
@@ -18,7 +32,6 @@ export default function Navbar() {
     }
   }, []);
 
-  // Toggle theme
   const toggleTheme = () => {
     if (dark) {
       document.documentElement.classList.remove("dark");
@@ -30,35 +43,3 @@ export default function Navbar() {
       setDark(true);
     }
   };
-
-  return (
-    <header className="w-full border-b border-gray-200 dark:border-neutral-800 bg-white/50 dark:bg-neutral-900/50 backdrop-blur-md sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-4 py-3">
-
-        {/* LEFT */}
-        <div className="flex items-center gap-3">
-          <MobileSidebar />
-          <Link href="/" className="text-xl font-semibold">
-            DevVelocity
-          </Link>
-        </div>
-
-        {/* RIGHT */}
-        <div className="hidden md:flex items-center gap-4">
-  <SearchBar />
-          <button
-            onClick={toggleTheme}
-            className="p-2 rounded-md hover:bg-gray-100 dark:hover:bg-neutral-800 transition"
-          >
-            {dark ? (
-              <IoSunny size={20} className="text-yellow-300" />
-            ) : (
-              <IoMoon size={20} className="text-gray-700" />
-            )}
-          </button>
-        </div>
-
-      </div>
-    </header>
-  );
-}
