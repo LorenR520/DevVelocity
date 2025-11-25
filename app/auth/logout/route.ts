05bc96b554ffede316c27fa8c6cd1c65eac2a5c9
LorenR520 <lorenrivas527@gmail.com>
2025-11-24 22:48:52 -0500

    Update route.ts

--- app/auth/logout/route.ts
+++ app/auth/logout/route.ts
@@ -1,7 +1,7 @@
+import { supabase } from "../../../lib/supabase";
 import { NextResponse } from "next/server";
-import { supabase } from "@/lib/supabase";
 
 export async function GET() {
   await supabase.auth.signOut();
-  return NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_SITE_URL));
+  return NextResponse.redirect("/");
 }
