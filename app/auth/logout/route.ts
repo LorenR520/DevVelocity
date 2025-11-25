d3449f44a679011ccbcdfb910bfb9b6ce6c491a0
LorenR520 <lorenrivas527@gmail.com>
2025-11-25 18:48:32 -0500

    Delete app/auth/logout/route.ts

--- app/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { supabase } from "../../../lib/supabase";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  await supabase.auth.signOut();
-  return NextResponse.redirect("/");
-}
