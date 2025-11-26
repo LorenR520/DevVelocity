6d975618abf2229643bbd1b90b39e150d7d8d180
LorenR520 <lorenrivas527@gmail.com>
2025-11-26 11:50:25 -0500

    Update route.ts

--- app/auth/logout/route.ts
+++ app/auth/logout/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getServerClient } from "../../../lib/supabase-server";
+import { supabase } from "@/lib/supabase";
 
 export const dynamic = "force-dynamic"; // 🚨 required
 
