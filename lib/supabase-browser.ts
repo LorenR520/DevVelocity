89764043b72327302592bfce08309a266bff116c
LorenR520 <lorenrivas527@gmail.com>
2025-11-26 11:13:13 -0500

    Create supabase-browser.ts

---
+++ lib/supabase-browser.ts
@@ -0,0 +1,11 @@
+// lib/supabase-browser.ts
+"use client";
+
+import { createClient } from "@supabase/supabase-js";
+
+export function getBrowserClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+  );
+}
